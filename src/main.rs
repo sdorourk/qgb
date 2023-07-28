1b53d37b82191d25381284b00631c61494468835
Sean O'Rourke <108634960+sdorourk@users.noreply.github.com>
2023-07-27 19:56:44 -0600

    Added initial debugger implmentation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{fs, path::PathBuf};
+use std::{
+    collections::HashSet,
+    fs,
+    io::{self, Write},
+    path::PathBuf,
+};
 
 use clap::Parser;
 
@@ -22,7 +27,7 @@ fn main() {
     let rom = fs::read(&cli.program).unwrap();
     let boot_rom = fs::read(&cli.boot_rom).unwrap();
 
-    let mut gb = match qgb::GameBoy::new(&rom, &boot_rom) {
+    let gb = match qgb::GameBoy::new(&rom, &boot_rom) {
         Ok(gb) => gb,
         Err(qgb::BootError::BootRomError(e)) => {
             eprintln!("'{}': {}", cli.boot_rom.display(), e);
@@ -34,16 +39,8 @@ fn main() {
         }
     };
 
-    loop {
-        println!("{:#?}", gb.cpu());
-        println!("Instruction: {}", gb.fetch().unwrap().opcode);
-        let mut buffer = String::new();
-        std::io::stdin().read_line(&mut buffer).unwrap();
-        if buffer.trim().to_ascii_lowercase() == "exit" {
-            break;
-        }
-        gb.step();
-    }
+    let mut debugger = Debugger::new(gb);
+    debugger.start();
 }
 
 fn init_logger() {
@@ -64,6 +61,109 @@ fn init_logger() {
         eprintln!(
             "an error occurred initializing the tracing library: {}",
             err
+        );
+    }
+}
+
+struct Debugger {
+    gb: qgb::GameBoy,
+    break_points: HashSet<u16>,
+}
+
+#[derive(Debug)]
+enum Command {
+    Dump,
+    Step,
+    Run(usize),
+    Exit,
+}
+
+impl TryFrom<String> for Command {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let cmd: Vec<String> = value
+            .trim()
+            .to_lowercase()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect();
+        if cmd.is_empty() {
+            return Err("No command specified".into());
+        }
+        match cmd[0].as_str() {
+            "dump" | "d" => Ok(Command::Dump),
+            "step" | "s" => Ok(Command::Step),
+            "run" | "r" => {
+                if cmd.len() == 1 {
+                    Ok(Command::Run(0))
+                } else if cmd.len() == 2 {
+                    if let Ok(step_size) = cmd[1].parse() {
+                        Ok(Command::Run(step_size))
+                    } else {
+                        Err("Unrecognized step size".into())
+                    }
+                } else {
+                    Err("Incorrect number of arguments specified".into())
+                }
+            }
+            "exit" | "quit" | "e" | "q" => Ok(Command::Exit),
+            _ => Err("Invalid command".into()),
+        }
+    }
+}
+
+impl Debugger {
+    pub fn new(gb: qgb::GameBoy) -> Self {
+        Self {
+            gb,
+            break_points: HashSet::new(),
+        }
+    }
+
+    pub fn start(&mut self) {
+        println!("Initializing debugger");
+        loop {
+            self.prologue();
+            print!("qgb> ");
+            io::stdout().flush().unwrap();
+
+            let mut buffer = String::new();
+            io::stdin().read_line(&mut buffer).unwrap();
+            let cmd = match Command::try_from(buffer) {
+                Ok(cmd) => cmd,
+                Err(msg) => {
+                    println!("{}", msg);
+                    continue;
+                }
+            };
+
+            match cmd {
+                Command::Dump => println!("{:#X?}", self.gb.cpu()),
+                Command::Step => {
+                    self.gb.step();
+                }
+                Command::Run(mut steps) => {
+                    if steps == 0 {
+                        steps = usize::MAX;
+                    }
+                    for step in 0..steps {
+                        self.gb.step();
+                        if step != steps - 1 {
+                            self.prologue();
+                        }
+                    }
+                }
+                Command::Exit => return,
+            }
+        }
+    }
+
+    fn prologue(&self) {
+        println!(
+            "{:04X}: {}",
+            self.gb.cpu().pc,
+            self.gb.fetch().unwrap().opcode
         );
     }
 }
